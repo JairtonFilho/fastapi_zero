0010a8c47c62df921e4ad60bb6d50d69866f742d
Jairton Filho <jsff@cin.ufpe.br>
2025-06-12 17:22:02 -0300

    CHORE: added get, put and delete users

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,10 +1,11 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from fastapi_zero.schemas import (
     Message,
     UserDB,
+    UserList,
     UserPublic,
     UserSchema,
 )
@@ -26,3 +27,44 @@ def create_user(user: UserSchema):
     database.append(user_with_id)
 
     return user_with_id
+
+
+@app.get('/users/', response_model=UserList, status_code=HTTPStatus.OK)
+def read_users():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found!'
+        )
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found!'
+        )
+    return database.pop(user_id - 1)
+
+
+@app.get(
+    '/users/{user_id}', response_model=UserPublic, status_code=HTTPStatus.OK
+)
+def read_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found!'
+        )
+    return database[user_id - 1]
